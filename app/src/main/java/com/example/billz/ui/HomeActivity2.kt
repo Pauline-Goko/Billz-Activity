125cab8f968d2842b2491efc6c01262fd27e74ab
Pauline-Wanjiru <gokowanjiru@gmail.com>
2023-08-22 19:20:45 +0300

    added a menu

--- app/src/main/java/com/example/billz/ui/HomeActivity2.kt
+++ app/src/main/java/com/example/billz/ui/HomeActivity2.kt
@@ -17,15 +17,49 @@ class HomeActivity2 : AppCompatActivity() {
         binding = ActivityHome2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val sharedprefs = getSharedPreferences(Constants.PREFS, Context.MODE_PRIVATE)
-        val logoutBtn = findViewById<Button>(R.id.btnLogOut)
-        logoutBtn.setOnClickListener{
-            val editor = sharedprefs.edit()
-            editor.clear()
-            editor.apply()
-            val loginActivity = Intent(this@HomeActivity2, LoginActivity::class.java)
-            startActivity(loginActivity)
-            finish()
+//        val sharedprefs = getSharedPreferences(Constants.PREFS, Context.MODE_PRIVATE)
+//        val logoutBtn = findViewById<Button>(R.id.btnLogOut)
+//        logoutBtn.setOnClickListener{
+//            val editor = sharedprefs.edit()
+//            editor.clear()
+//            editor.apply()
+//            val loginActivity = Intent(this@HomeActivity2, LoginActivity::class.java)
+//            startActivity(loginActivity)
+//            finish()
+//        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupBottomNav()
+
+    }
+
+    fun setupBottomNav(){
+        binding.bnvHome.setOnItemReselectedListener { menuItem->
+           when(menuItem.itemId){
+                R.id.summary -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, SummaryFragment()).commit()
+                    true
+                }
+               R.id.upcoming -> {
+                   supportFragmentManager.beginTransaction().replace(R.id.fcvHome, UpcomingBillsFragment()).commit()
+                   true
+               }
+               R.id.paid -> {
+                   supportFragmentManager.beginTransaction().replace(R.id.fcvHome, PaidBillsFragment()).commit()
+                   true
+               }
+               R.id.settings -> {
+                   supportFragmentManager.beginTransaction().replace(R.id.fcvHome, SettingsFragment()).commit()
+                   true
+               }
+              else -> false
+
+
+            }
         }
     }
 }
